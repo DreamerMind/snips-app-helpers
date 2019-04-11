eec2677f13b0d256cbb17ad214a785b8cde6f842
Julien Balian <julien.balian@snips.ai>
2019-04-11 11:54:15 +0200

    fix: add middleware logging

--- src/actions/snips-app-middleware/action-middleware.py
+++ src/actions/snips-app-middleware/action-middleware.py
@@ -1,19 +1,31 @@
 #!/usr/bin/env python3
+
 """
 This module contains a Snips app that act as a middleware beween intent and
 slots to remap.
-
-mosquitto_pub -h localhost -t hermes/intent/mathproblem -m '{"intent": {"intentName": "mathproblem", "probability": 1.0}, "slots": [{"slotName": "blah", "value": "bavardage"}], "sessionId": "asdisaid", "siteId": "default"}'
-
 """
 
 from configparser import ConfigParser
 from copy import deepcopy
 import pathlib
 import json
+import logging
 
 import toml
 import paho.mqtt.client as mqtt
+
+
+log = logging.getLogger("snips-middleware")
+log.setLevel(logging.DEBUG)
+formatter = logging.Formatter(
+    "%(asctime)s - %(name)s - %(filename)s:%(lineno)d %(levelname)-10s %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+handler = logging.RotatingFileHandler('./middleware.log', encoding='utf-8')
+handler.setLevel(logging.DEBUG)
+handler.setFormatter(formatter)
+log.addHandler(handler)
+log.propagate = False
 
 
 CONFIGURATION_ENCODING_FORMAT = "utf-8"
@@ -68,7 +80,7 @@ class AssistantMiddleware(object):
         self.client = mqtt.Client("snips-middleware")
         self.client.on_connect = self._subscribe_reroute
         if self.debug:
-            print("debug mode activated")
+            log.info("debug mode activated")
             self.client.on_message = self.emit_rerouted_intent_debug
 
         self.client.connect(mqtt_host, mqtt_port, mqtt_timeout)
@@ -79,14 +91,14 @@ class AssistantMiddleware(object):
         if self.debug:
             self.client.subscribe("hermes/intent/#", 1)
         for intent_name, new_intent_name in self.route_intents.items():
-            print('hook re route intent : %s => %s' % (intent_name, new_intent_name))
+            log.info('hook re route intent : %s => %s', intent_name, new_intent_name)
             client.message_callback_add(
                 'hermes/intent/%s' % intent_name, self.emit_rerouted_intent)
 
     def emit_rerouted_intent_debug(self, client, userdata, message):
-        print("debug => captured intent")
-        print(str(message.topic))
-        print(str(message.payload))
+        log.debug("debug => captured intent")
+        log.debug("topic: %s", str(message.topic))
+        log.debug("payload: %s", str(message.payload))
 
     def emit_rerouted_intent(self, client, userdata, msg):
         payload = json.loads(msg.payload.decode('utf8'))
@@ -99,10 +111,12 @@ class AssistantMiddleware(object):
                 new_slot_name = self.route_slots[slot_name]
                 new_payload['slots'][new_slot_name] = slot_content
                 del new_payload['slots'][slot_name]
-        print("Intent {0} detected with slots {1}".format(name, [s['slotName'] for s in payload['slots']]))
-        print("Rerouted to Intent {0} detected with slots {1}".format(
-            rerouted_intent_name, [s['slotName'] for s in new_payload['slots']]
-        ))
+        log.info("Intent %s detected with slots %s",
+                 name,
+                 [s['slotName'] for s in payload['slots']])
+        log.info("Rerouted to Intent %s detected with slots %s",
+                 rerouted_intent_name,
+                 [s['slotName'] for s in new_payload['slots']])
         client.publish(
             'hermes/intent/%s' % rerouted_intent_name,
             json.dumps(new_payload)
